954b9f29436b3b2e84a81e5d3afa40889104c1f8
Patrik Hrmo <patohrmo@gmail.com>
2016-09-19 17:02:11 +0200

    implemented some http things
    all promises, not observables yet

--- app/hero.service.js
+++ app/hero.service.js
@@ -8,13 +8,21 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-var mock_heroes_1 = require('./mock-heroes');
 var core_1 = require('@angular/core');
+var http_1 = require('@angular/http');
+require('rxjs/add/operator/toPromise');
 var HeroService = (function () {
-    function HeroService() {
+    function HeroService(http) {
+        this.http = http;
     }
+    HeroService.prototype.handleError = function (error) {
+        console.error('An error occurred', error);
+        return Promise.reject(error.message || error);
+    };
     HeroService.prototype.getHeroes = function () {
-        return Promise.resolve(mock_heroes_1.HEROES);
+        return this.http.get("server/heroes.php").toPromise()
+            .then(function (response) { return response.json().data; })
+            .catch(this.handleError);
     };
     HeroService.prototype.getHeroesSlowly = function () {
         var _this = this;
@@ -29,7 +37,7 @@ var HeroService = (function () {
     };
     HeroService = __decorate([
         core_1.Injectable(), 
-        __metadata('design:paramtypes', [])
+        __metadata('design:paramtypes', [http_1.Http])
     ], HeroService);
     return HeroService;
 }());
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -2,11 +2,22 @@
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Injectable } from '@angular/core';
-
+import {Headers, Http} from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 @Injectable()
 export class HeroService {
+  constructor(private http: Http){}
+
+  private handleError(error: any): Promise<any> {
+  console.error('An error occurred', error); // for demo purposes only
+  return Promise.reject(error.message || error);
+  }
+
   getHeroes(): Promise<Hero[]> {
-    return Promise.resolve(HEROES);
+    return this.http.get("server/heroes.php").toPromise()
+    .then(response => response.json().data as Hero[])
+    .catch(this.handleError);
+
   }
 
   getHeroesSlowly(): Promise<Hero[]> {
